094b6cd7ffeefb0a8e37fcee953d2402a37501d1
Krzysztof Pado <povder@gmail.com>
2017-01-08 13:00:34 +0100

    Use rdbc-pgsql 0.3.0.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,5 +2,5 @@ import sbt._
 
 object Library {
   val rdbcScalaApi = "io.rdbc" %% "rdbc-api-scala" % "0.0.38"
-  val rdbcPgsqlNetty = "io.rdbc.pgsql" %% "pgsql-transport-netty" % "0.1.2"
+  val rdbcPgsqlNetty = "io.rdbc.pgsql" %% "pgsql-transport-netty" % "0.3.0.4"
 }
