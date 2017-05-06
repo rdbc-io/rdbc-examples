94acda708849cfad7b3b9067f18f414e319338cf
Krzysztof Pado <povder@gmail.com>
2017-05-06 14:55:33 +0200

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,6 @@
 import sbt._
 
 object Library {
-  val rdbcScalaApi = "io.rdbc" %% "rdbc-api-scala" % "0.0.45"
-  val rdbcPgsqlNetty = "io.rdbc.pgsql" %% "pgsql-transport-netty" % "0.3.0.8"
+  val rdbcScalaApi = "io.rdbc" %% "rdbc-api-scala" % "0.0.48"
+  val rdbcPgsqlNetty = "io.rdbc.pgsql" %% "pgsql-transport-netty" % "0.3.0.9"
 }
